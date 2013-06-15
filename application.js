be154b27e1fe5e5088cd590884281213a7eb4721
Simon Ljungberg <hi@iamsim.me>
2013-06-15 10:41:01 +0200

    Started working on the main application object.

---
+++ application.js
@@ -0,0 +1,55 @@
+//
+// # Application
+//
+
+var util   = require('util');
+var events = require('events');
+
+//
+// ## Application Constructor
+//
+var Application = function () {
+  events.EventMitter.call(this);
+
+  this.controllers = {};
+};
+
+// Extend event emitter.
+util.inherits(Application, events.EventEmitter);
+
+//
+// ## Set Controller
+//
+// * **name**, the name of the controller.
+// * **controller**, the actual controller.
+//
+// **Returns** `false` if a controller by that name already exists.
+//
+Application.prototype.setController = function (name, controller) {
+  var ret = false;
+  if (!this.controllers[name]) {
+    this.controllers[name] = controller;
+    ret = true;
+  }
+
+  return ret;
+};
+
+//
+// ## Get Controller
+//
+// * **name**, the name of the controller.
+//
+// **Returns** a controller or `false` if controller does not exist.
+//
+Application.prototype.getController = function (name) {
+  var ret = false;
+  if (this.controllers[name]) {
+    ret = this.controllers[name];
+  }
+
+  return ret;
+};
+
+module.exports = Application;
+
